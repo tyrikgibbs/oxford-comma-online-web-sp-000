18e80327a383b1b1e35bafe1bca1e818e4daf149
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-28 00:05:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join (" and ")
+  elsif array.length == 3
+    array[-1].prepend "and "
+    array.join (", ")
+  else
+    array[-1].prepend "and "
+    array.join (", ")
+  end
 end
